d052434c9382cb079d791c3c7084881d60328644
Landan <102269895+LandanJH@users.noreply.github.com>
2025-02-27 10:48:59 -0600

    Update Lockbox_linux_default.sh

--- Lockbox/scripts/Lockbox_linux_default.sh
+++ Lockbox/scripts/Lockbox_linux_default.sh
@@ -11,8 +11,12 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-ipv6() {
+if [ "$#" -ne 2 ]; then
+    echo -e "${RED}[!] Usage: $0 <old_password> <new_password>"
+    exit 1
+fi
 
+ipv6() {
     # Disable IPv6 by modifying sysctl configuration
     echo -e "${YELLOW}[!] Disabling IPv6 via sysctl... ${NC}"
     echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
@@ -36,13 +40,11 @@ check() {
         echo -e "${BLUE} conntrack is not loaded, installing and configuring... ${NC}"
         sudo apt install conntrack -y
         echo "nf_conntrack" | sudo tee -a /etc/modules
-        sudo modprobe nf_conntrack_ftp
+        sudo modprobe nf_conntrack_ftp  ## CONFIGURE THIS FURTHER FOR Lockbox
     else
         echo -e "${BLUE} conntrack is loaded, configuring... ${NC}"
         ## COMMANDS
     fi
-
-
 }
 
 rules() {
@@ -53,7 +55,7 @@ rules() {
 
     # contrack stuff
     sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED, RELATED -j ACCEPT
-    sudo iptables -A INPUT -m conntrack --ctstate NEW -j DROP
+    #sudo iptables -A INPUT -m conntrack --ctstate NEW -j DROP # need to see if this is allowed???
 
     # other
 }
@@ -62,15 +64,46 @@ save() {
     sudo apt install iptables-persistent -y
     sudo service iptables save
     sudo iptables-save > /etc/iptables/rules.v4
+    sudo chattr +i /etc/iptables/rules.v4
+    echo -e "${BLUE}  [*] Reminder that you will need to unchattr this file to change it ${NC}"
+}
+
+accounts() {
+
+    old_password="$1"
+    new_password="$2"
+
+    account_names=("test1" "test2")
+    #account_names=("goon1" "goon2" "hacker" "buyer" "lockpick" "safecracker" "root")
+    #account_names=("goon1" "goon2" "hacker" "buyer" "lockpick" "safecracker" "getaway" "driver" "watchdog" "manager" "mastermind" "root")
+    for account_name in "${account_names[@]}"; do
+        echo "Changing password for $account_name..."
+        
+        # Use chpasswd to change password
+        echo "$account_name:$new_password" | sudo chpasswd
+        
+        if [ $? -eq 0 ]; then
+            echo "Password changed for $account_name"
+        else
+            echo "Failed to change password for $account_name"
+        fi
+    done
+
+}
+
+clean() {
+    sudo history -c && history -w
 }
 
 main() {
     ipv6
     check
     rules
+    accounts "$1" "$2"
     save
+    clean
     echo -e "${GREEN}[*] default configurations completed! ${NC}"
     echo -e "${RED}[!!!] YOU WILL NEED TO REBOOT THE BOX FOR CONFIGS TO SAVE ${NC}"
 }
 
-main
+main "$1" "$2"
